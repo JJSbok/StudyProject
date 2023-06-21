dcd635202447b20701edf1bb01fcf813398a1300
JJSbok <sprpsa33@naver.com>
2023-06-21 17:25:02 +0900

    0621

--- b4-main/src/main/java/org/zerock/b4/controller/ProductController.java
+++ b4-main/src/main/java/org/zerock/b4/controller/ProductController.java
@@ -8,12 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.zerock.b4.dto.ProductRegisterDTO;
+import org.zerock.b4.service.ProductService;
 
 @Controller
 @Log4j2
 @RequiredArgsConstructor
 @RequestMapping("/product")
 public class ProductController {
+
+  private final ProductService productService;
 
   @GetMapping("/register")
   public void register(){
@@ -26,6 +29,10 @@ public class ProductController {
 
     log.info("---------------------");
     log.info(registerDTO);
+
+    Integer pno = productService.register(registerDTO);
+
+    log.info("NEW PNO : " + pno);
 
     return "redirect:/product/list";
   }
