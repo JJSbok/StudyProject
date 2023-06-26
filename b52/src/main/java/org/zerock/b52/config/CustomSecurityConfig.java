42fa87d7e9bc6855a07fd5bf80adbc945cab8c42
JJSbok <sprpsa33@naver.com>
2023-06-26 15:51:32 +0900

    0626

--- b52/src/main/java/org/zerock/b52/config/CustomSecurityConfig.java
+++ b52/src/main/java/org/zerock/b52/config/CustomSecurityConfig.java
@@ -1,5 +1,7 @@
 package org.zerock.b52.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,34 +10,64 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.zerock.b52.security.handler.CustomAccessDeniedHandler;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Configuration
 @Log4j2
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class CustomSecurityConfig {
 
-  @Bean
-  public PasswordEncoder passwordEncoder(){
-    return new BCryptPasswordEncoder();
-  }
-  
+    private final DataSource dataSource;
+
     @Bean
-    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
-    
-      log.info("filter chain--------------------------------");
-
-      // http.formLogin(Customizer.withDefaults());
-      // /login 경로 로그인 페이지 띄우기
-      http.formLogin(config -> {
-        config.loginPage("/member/signin");
-      });
-
-      http.csrf(config -> {
-        config.disable();
-      });
-
-      return http.build();
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+        repo.setDataSource(dataSource);
+        return repo;
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception{
+
+        log.info("filter chain------------------------------------");
+
+        // http.formLogin(Customizer.withDefaults());
+        // /login 경로 로그인 페이지 띄우기
+        http.formLogin(config -> {
+            config.loginPage("/member/signin");
+        });
+
+        http.exceptionHandling(
+            config -> config.accessDeniedHandler(new CustomAccessDeniedHandler())
+            );
+
+        http.rememberMe(config -> {
+            // 리멤버미는 자동로그인. 쿠키를 다룬다
+            // 쿠키의 유효시간(초단위)
+            config.tokenRepository(persistentTokenRepository());
+            config.tokenValiditySeconds(60*60*24*7);
+        });
+
+        http.csrf(config -> {
+            config.disable();
+        });
+
+        http.oauth2Login(config -> {
+            config.loginPage("/member/signin");
+        });
+
+        return http.build();
+    }
+
 }
